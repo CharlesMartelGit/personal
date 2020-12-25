c63f09e8cda18dc7c0b927ac6a6d1f7eadfc72a9
Charles Martel <charles.martel@polymtl.ca>
2020-12-25 13:00:02 -0500

    added basic form UI

--- socket-io-client/src/App.js
+++ socket-io-client/src/App.js
@@ -1,6 +1,10 @@
 import React, { useState, useEffect } from "react";
 import openSocket from "socket.io-client";
 import tesseract from "./tesseract.gif"
+import { Button, Container, Form } from "react-bootstrap"
+import "bootstrap/dist/css/bootstrap.min.css"
+import "./App.css"
+
 const socket = openSocket('http://localhost:4001', {transports: ['websocket']});
 
 function App() {
@@ -15,12 +19,25 @@ function App() {
   }, []);
 
   return (
-    <>
-    <p>
-      {response}
-    </p>
-    <img src={tesseract} alt="tesseract"/>
-    </>
+    <div className="App">
+    <header className="App-header">
+      <Container>
+        <img src={tesseract} className="Tesseract" alt="tesseract"/>
+        <Form>
+          <Form.Group>
+            <Form.Label>Email Address</Form.Label>
+            <Form.Control type="email" placeholder="example@email.com"></Form.Control>
+            <Form.Text className="text-muted"></Form.Text>
+          </Form.Group>
+          <Form.Group>
+            <Form.Label>Password</Form.Label>
+            <Form.Control type="password" placeholder="password"></Form.Control>
+            <Form.Text className="text-muted"></Form.Text>
+          </Form.Group>
+        </Form>
+      </Container>
+    </header>
+    </div>
   );
 }
 
