397820bb7f9a87403131d26231733c6bf718d1a8
Charles Martel <charles.martel@polymtl.ca>
2020-12-26 20:02:35 -0500

    new home page icons and buttons

--- socket-io-client/src/HomePage.js
+++ socket-io-client/src/HomePage.js
@@ -1,6 +1,10 @@
 import React from "react"
+import manage from "./resources/manage.png"
+import projects from "./resources/projects.png"
+import tickets from "./resources/tickets.png"
+import "./App.css"
 import auth from "./auth.js"
-import { Button } from "react-bootstrap"
+import { Alert, Button, Col, Container, Form, Row } from "react-bootstrap"
 import { useHistory } from "react-router-dom"
 
 function HomePage() {
@@ -13,7 +17,34 @@ function HomePage() {
 
   return (
     <div className="App">
-      <Button variant="secondary" onClick={handleClick}>Login</Button>
+      <header className="App-header">
+        <Container fluid>
+          <Row className="Row">
+              <Alert className="Field" variant="secondary">Currently signed in as : Charles</Alert>
+              <Button className="Field" variant="primary" onClick={handleClick}>Logout</Button>
+          </Row>
+          <Row>
+            <Col>
+              <Button variant="secondary">
+                <img src={manage} className="HomeIcon" alt="manage"/>
+              </Button>
+              <Alert variant="info">Manage</Alert>
+            </Col>
+            <Col>
+              <Button variant="secondary">
+                <img src={projects} className="HomeIcon" alt="projects"/>
+              </Button>
+              <Alert variant="info">Projects</Alert>
+            </Col>
+            <Col>
+              <Button variant="secondary">
+                <img src={tickets} className="HomeIcon" alt="tickets"/>
+              </Button>
+              <Alert variant="info">Projects</Alert>
+            </Col>
+          </Row>
+        </Container>
+      </header>
     </div>
   );
 }
--- socket-io-client/src/LoginPage.js
+++ socket-io-client/src/LoginPage.js
@@ -1,5 +1,5 @@
 import React from "react"
-import tesseract from "./tesseract.gif"
+import tesseract from "./resources/tesseract.gif"
 import "./App.css"
 import auth from "./auth.js"
 import { Alert, Button, Container, Form } from "react-bootstrap"
@@ -15,25 +15,25 @@ function LoginPage() {
 
   return (
     <div className="App">
-    <header className="App-header">
-      <Container>
-        <Alert variant="primary">Charles' Bug Tracker</Alert>
-        <img src={tesseract} className="Tesseract" alt="tesseract"/>
-        <Form>
-          <Form.Group>
-            <Form.Label>Email Address</Form.Label>
-            <Form.Control type="email" placeholder="example@email.com"></Form.Control>
-            <Form.Text className="text-muted"></Form.Text>
-          </Form.Group>
-          <Form.Group>
-            <Form.Label>Password</Form.Label>
-            <Form.Control type="password" placeholder="password"></Form.Control>
-            <Form.Text className="text-muted"></Form.Text>
-          </Form.Group>
-        </Form>
-        <Button variant="secondary" type="submit" onClick={handleClick}>Login</Button>
-      </Container>
-    </header>
+      <header className="App-header">
+        <Container>
+          <Alert variant="primary">Charles' Bug Tracker</Alert>
+          <img src={tesseract} className="Tesseract" alt="tesseract"/>
+          <Form>
+            <Form.Group>
+              <Form.Label>Email Address</Form.Label>
+              <Form.Control type="email" placeholder="example@email.com"></Form.Control>
+              <Form.Text className="text-muted"></Form.Text>
+            </Form.Group>
+            <Form.Group>
+              <Form.Label>Password</Form.Label>
+              <Form.Control type="password" placeholder="password"></Form.Control>
+              <Form.Text className="text-muted"></Form.Text>
+            </Form.Group>
+          </Form>
+          <Button variant="secondary" type="submit" onClick={handleClick}>Login</Button>
+        </Container>
+      </header>
     </div>
   );
 }
